0b2bb5812f985b4366d17635eb8481d36792a678
leo <leoplayz317@gmail.com>
2025-08-17 13:30:36 -0400

    initial commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,61 @@
+import { env } from "cloudflare:workers";
+import { RPCHandler } from "@orpc/server/fetch";
+import { createContext } from "./lib/context";
+import { appRouter } from "./routers/index";
+import { auth } from "./lib/auth";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { streamText, convertToModelMessages } from "ai";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+
+const app = new Hono();
+
+app.use(logger());
+app.use(
+  "/*",
+  cors({
+    origin: env.CORS_ORIGIN || "",
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
+
+const handler = new RPCHandler(appRouter);
+app.use("/rpc/*", async (c, next) => {
+  const context = await createContext({ context: c });
+  const { matched, response } = await handler.handle(c.req.raw, {
+    prefix: "/rpc",
+    context: context,
+  });
+
+  if (matched) {
+    return c.newResponse(response.body, response);
+  }
+  await next();
+});
+
+
+
+app.post("/ai", async (c) => {
+  const body = await c.req.json();
+  const uiMessages = body.messages || [];
+  const google = createGoogleGenerativeAI({
+    apiKey: env.GOOGLE_GENERATIVE_AI_API_KEY,
+  });
+  const result = streamText({
+    model: google("gemini-1.5-flash"),
+    messages: convertToModelMessages(uiMessages),
+  });
+
+  return result.toUIMessageStreamResponse();
+});
+
+app.get("/", (c) => {
+  return c.text("OK");
+});
+
+export default app;
