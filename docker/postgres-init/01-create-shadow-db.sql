8806b58e2cb10df6cf973bfbe421c7603746313f
Leo <leoplayz317@gmail.com>
2025-10-08 18:39:29 -0400

    fix

--- docker/postgres-init/01-create-shadow-db.sql
+++ docker/postgres-init/01-create-shadow-db.sql
@@ -1,9 +1,14 @@
-DO $$
-BEGIN
-	IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'openchat') THEN
-		EXECUTE 'CREATE DATABASE openchat';
-	END IF;
-	IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'openchat_shadow') THEN
-		EXECUTE 'CREATE DATABASE openchat_shadow';
-	END IF;
-END$$;
+\set main_db 'openchat'
+\set shadow_db 'openchat_shadow'
+
+-- Create the main application database if it does not already exist.
+SELECT format('CREATE DATABASE %I OWNER %I', :'main_db', current_user)
+WHERE NOT EXISTS (
+	SELECT 1 FROM pg_database WHERE datname = :'main_db'
+)\gexec
+
+-- Create the shadow database used by Drizzle migrations if needed.
+SELECT format('CREATE DATABASE %I OWNER %I', :'shadow_db', current_user)
+WHERE NOT EXISTS (
+	SELECT 1 FROM pg_database WHERE datname = :'shadow_db'
+)\gexec
